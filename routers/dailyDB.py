737f4802f2d0c8ad78ffc55d485880d2a4a83e15
Johnathan Carranza <82400181+JohnCarra@users.noreply.github.com>
2022-04-01 17:42:27 -0700

    DB updated with PK
    PK ID of type INT added to auto incremenet

--- routers/dailyDB.py
+++ routers/dailyDB.py
@@ -6,11 +6,13 @@ words = json.load(answers)
 words = [(x,) for x in words]
 
 
-db = sqlite3.connect("db")
+db = sqlite3.connect("checkdb")
 cursor = db.cursor()
 cursor.execute(
     '''CREATE TABLE IF NOT EXISTS dailyWord( id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT)''')
-query = '''INSERT INTO answers (word) VALUES (?);'''
+cursor.execute(
+    '''CREATE TABLE IF NOT EXISTS userGuess( id INTEGER PRIMARY KEY AUTOINCREMENT, guess TEXT)''')
+query = '''INSERT INTO dailyword (word) VALUES (?);'''
 cursor.executemany(query, words)
 db.commit()
 print(cursor.rowcount)
