951cde6ea4aa978efb64a57ec24bf993026b33d5
Johnathan Carranza <82400181+JohnCarra@users.noreply.github.com>
2022-04-05 12:47:43 -0700

    Delete End Point
    startup sh modified to startup validation service.

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 # TODO: add all python and DB commands that need to be run
 # and then Procfile
-foreman start -e .env
+#foreman start -e .env
+cd Word_Validation_Service
+python3 wordsDB.py
+uvicorn wordsAPI:app --reload
