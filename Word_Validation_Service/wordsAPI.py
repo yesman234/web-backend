276abb59c816c2ba9d1cf304bdee1c424a3442ef
Daniel Lungaro <chunk_77@hotmail.com>
2022-04-04 20:57:53 -0700

    basic impl working

--- Word_Validation_Service/wordsAPI.py
+++ Word_Validation_Service/wordsAPI.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+import contextlib
+import sqlite3
+from typing import List
 
 app = FastAPI()
 
 WORDLE_WORD_LENGTH = 5
 
-@app.get("/words/validate/{word}")
-async def validate_word(word: str):
-    if len(word) == WORDLE_WORD_LENGTH:
-        return {"result_valid": True}
-    return {"result_valid": False}
+class Guess(BaseModel):
+    word: str
+
+def get_db():
+    with contextlib.closing(sqlite3.connect("WordValidationDB")) as db:
+        db.row_factory = sqlite3.Row
+        yield db
+
+@app.post("/WordValidations")
+def validate_word(guess: Guess,  db: sqlite3.Connection = Depends(get_db)):
+    cur = db.execute(
+        """
+        SELECT word FROM gameDictionary WHERE word = (?);
+        """
+        , (guess.word, )
+    )
+    word_found = cur.fetchall()
+
+    return { "word_valid": True } if word_found else { "word_valid": False }
