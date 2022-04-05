ccd92c06133fd99c7e83db53ef9234a4e9ac4105
Johnathan Carranza <82400181+JohnCarra@users.noreply.github.com>
2022-04-05 15:51:58 -0700

    updated startup script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 # TODO: add all python and DB commands that need to be run
 # and then Procfile
-#foreman start -e .env
-cd Word_Validation_Service
-python3 wordsDB.py
-uvicorn wordsAPI:app --reload
+
+python3 Word_Validation_Service/wordsDB.py
+uvicorn --port $PORT Word_Validation_Service.wordsAPI:app --reload
+foreman start
