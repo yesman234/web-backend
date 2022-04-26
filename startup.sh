d597e977d9ef20f575a27bf2b3406c6eb1243926
Daniel Lungaro <chunk_77@hotmail.com>
2022-04-25 21:39:20 -0700

    fix procfile lingering quote

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-python Word_Validation_Service/wordsDB.py
-python Stats_Service/database/stats_db.py
-python Stats_Service/database/fake_stat_data.py
+python3 Word_Validation_Service/wordsDB.py
+python3 Stats_Service/database/stats_db.py
+python3 Stats_Service/database/fake_stat_data.py
 foreman start
