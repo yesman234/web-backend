408d6839c19f46b3c87179a9f394ee4955515e9d
provmawn <provmawn@gmail.com>
2022-05-03 15:21:17 -0700

    implement update game state endpoint

--- Game_State/api.py
+++ Game_State/api.py
@@ -41,9 +41,23 @@ def start_a_new_game(new_game: NewGame):
 
 # Updating the state of a game. When a user makes a new guess for a game, record the guess and update the number of guesses remaining. If a user tries to guess more than six times, they should receive an error.
 # Note: you do not need to check whether the guess is valid, if the guess is correct, or report on the placement of the letters in the answer. This functionality was completed in Project 2.
-@app.put('/update')
-def update_state_of_a_game():
-    return {'hello': 'world'}
+@app.put('/update/{user_id}')
+def update_state_of_a_game(user_id: str, guess: str):
+    
+    # get gamestate object based on user_id
+    cur_gamestate: GameState = GameState.json_to_GameState(json.loads(r.get(user_id), object_hook=lambda d: SimpleNamespace(**d)))
+
+    # check if they don't have more guesses
+    if not cur_gamestate.eligible_guess():
+        return HTTPStatus.BAD_REQUEST
+
+    # record the guess
+    cur_gamestate.add_guess(guess)
+
+    # update the number of guesses remaining
+    r.set(cur_gamestate.user_id, json.dumps(cur_gamestate, default=vars))
+
+    return {user_id: json.dumps(cur_gamestate, default=vars)}
 
 # Restoring the state of a game. Upon request, the user should be able to retrieve an object containing the current state of a game, including the words guessed so far and the number of guesses remaining.
 @app.get('/restore')
