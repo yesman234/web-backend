762c0008d855a2c78ef74ca4074d1f6656823c3d
cc506 <cchung28@ucmerced.edu>
2022-05-06 17:02:43 -0700

    crontab implementation

--- startup.sh
+++ startup.sh
@@ -3,6 +3,9 @@ python3 Stats_Service/database/stats_db.py
 python3 Stats_Service/database/fake_stat_data.py
 redis-server --daemonize yes
 
+HOME_PATH=$(pwd)
+sudo /bin/bash -c 'echo "*/10 * * * * python3 $HOME_PATH/Stats_Service/update/update.py" >> /etc/crontab'
+
 foreman start -e .env -m validate=1,stats=1,answer=1,game_state=1 &
 
 ./traefik --configFile=traefik.toml
